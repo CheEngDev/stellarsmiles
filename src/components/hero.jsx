c46f1cecb7c1660ddf7f20601dda6b240571158a
chejaysdv <ChEjaysdv@gmail.com>
2023-07-23 13:30:05 +0800

    finished with nav and hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,20 +3,20 @@ import ad from "../assets/dental.mp4";
 
 const Hero = () => {
   return (
-    <div className="grid grid-cols-4 auto-cols-auto gap-2 ">
-      <div className="c ">
-        <h1 className="text-6xl font-medium tracking-wider mt-4 ml-2">
+    <div className="md:grid md:grid-cols-4 md:auto-cols-auto md:gap-2 ">
+      <div className="">
+        <h1 className=" text-4xl text-center font-medium tracking-wider mt-4 ml-2 md:text-left md:text-4xl lg:text-6xl">
           STELLAR SMILES
         </h1>
-        <div className="border-b-2 border-black ml-4 mr-8 mt-4"></div>
-        <p className="text-2xl mt-8 px-2 ">
+        <div className="border-b-2 mx-8 mt-2 border-black md:ml-3 md:mr-12 md:mt-2 lg:mt-4 lg:ml-4 lg:mr-8"></div>
+        <p className="text-2xl text-center px-6 mt-3 lg:text-2xl lg:mt-8 lg:px-2 md:text-lg md:mt-6 md:px-2 md:text-left">
           Radiant Smiles, One Appointment at a Time!
         </p>
-        <p className="text-lg font-light mt-5 px-2 ">
+        <p className="px-2 font-light text-center mt-3 lg:text-lg lg:mt-5 md:text-base md:mt-4 md:text-left">
           Call or Text to Book <br /> 523-7569 or book online
         </p>
-        <div className="mt-8 ml-7">
-          <a className="px-7 py-3 border-2 border-black cursor-pointer hover:border-[#cf945f] hover:text-[#cf945f]">
+        <div className="text-center mt-4 mb-4 md:ml-7 md:text-left lg:mt-8 md:mt-5 ">
+          <a className="px-4 py-2 border-2 border-black cursor-pointer hover:border-[#cf945f] hover:text-[#cf945f] lg:px-7 lg:py-3 md:px-5 md:py-2">
             BOOK NOW
           </a>
         </div>
@@ -26,7 +26,7 @@ const Hero = () => {
         loop
         muted
         src={ad}
-        className="max-w-full col-span-3 rounded-lg mt-2 pl-2"
+        className="max-w-full col-span-3  mt-2 md:pl-2"
       ></video>
     </div>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import stellar from "../assets/stellar.png";
+import { BiMenu } from "react-icons/bi";
+import { FaFacebookSquare } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
+  const [navopen, setnavopen] = useState(false);
+
+  function handleOpenNav() {
+    setnavopen(!navopen);
+  }
   return (
-    <div className="bg-black w-full h-[70px] flex justify-between items-center px-2 ">
-      <img className="w-[210px] -translate-y-2" src={stellar} alt="" />
-      <ul className=" text-white font-semibold text-xl md:hidden lg:flex">
-        <li className="mx-3 cursor-pointer hover:text-[#e6d2af] ">HOME</li>
-        <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">ABOUT US</li>
-        <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">SERVICES</li>
-        <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">INSURANCES</li>
-        <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">CONTACT US</li>
-      </ul>
+    <div>
+      <div className="bg-black w-full flex justify-between items-center px-2 h-[50px] lg:h-[70px] md:h-[50px]">
+        <img
+          className="w-[155px] -translate-y-2 lg:w-[210px]  md:w-[170px]"
+          src={stellar}
+          alt=""
+        />
+        <ul className=" text-white font-semibold hidden md:flex md:text-lg lg:text-xl">
+          <li className="mx-3 cursor-pointer hover:text-[#e6d2af] ">HOME</li>
+          <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">ABOUT US</li>
+          <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">SERVICES</li>
+          <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">
+            INSURANCES
+          </li>
+          <li className="mx-3 cursor-pointer hover:text-[#e6d2af]">
+            CONTACT US
+          </li>
+        </ul>
+        <div className="flex items-center">
+          <FaFacebookSquare
+            className="lg:mr-10 cursor-pointer"
+            fill="white"
+            size={25}
+          />
+          {!navopen ? (
+            <BiMenu
+              className="block mx-1 cursor-pointer md:hidden "
+              onClick={handleOpenNav}
+              fill="white"
+              size={35}
+            />
+          ) : (
+            <AiOutlineClose
+              className="block mx-1 cursor-pointer md:hidden "
+              onClick={handleOpenNav}
+              fill="white"
+              size={25}
+            />
+          )}
+        </div>
+      </div>
+      <div
+        className={
+          navopen
+            ? "bg-black opacity-80 absolute z-20 text-center w-full max-h-full overflow-hidden transition-all duration-1000 md:hidden"
+            : "bg-black opacity-80 absolute z-20 text-center w-full max-h-0 overflow-hidden transition-all duration-500"
+        }
+      >
+        <ul className=" text-white text-xl font-semibold static  z-10">
+          <li className="mx-3 my-2 cursor-pointer hover:text-[#e6d2af] ">
+            HOME
+          </li>
+          <li className="mx-3 my-2 cursor-pointer hover:text-[#e6d2af]">
+            ABOUT US
+          </li>
+          <li className="mx-3 my-2 cursor-pointer hover:text-[#e6d2af]">
+            SERVICES
+          </li>
+          <li className="mx-3 my-2 cursor-pointer hover:text-[#e6d2af]">
+            INSURANCES
+          </li>
+          <li className="mx-3 my-2 cursor-pointer hover:text-[#e6d2af]">
+            CONTACT US
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
