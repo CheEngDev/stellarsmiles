29c082678005b87b7427afb0e084ee9375fd0105
chejaysdv <ChEjaysdv@gmail.com>
2023-07-29 13:57:48 +0800

    added auth Service for login and logout and also added the jwt as a default in header in req

---
+++ src/services/authService.js
@@ -0,0 +1,25 @@
+import jwtDecode from "jwt-decode";
+import http from "./httpService";
+
+const apiEndpoint = "/auth";
+
+http.setJwt(getJwt());
+
+export async function login(user) {
+  const { data: jwt } = await http.post(apiEndpoint, user);
+  localStorage.setItem("token", jwt);
+  console.log(jwt);
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+}
+
+export function getJwt() {
+  return localStorage.getItem("token");
+}
+
+export default {
+  login,
+  logout,
+};
